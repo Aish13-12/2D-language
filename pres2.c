1f4d35455eafb6be2eb68ba15358bddea2c94806
Aish13-12 <54772803+Aish13-12@users.noreply.github.com>
2019-09-14 22:57:28 -0700

    Add files via upload

---
+++ pres2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main() //main function begins
+{
+
+double k_1,k_2 ,F;	
+double **p;
+F=-51;
+p=loadtxt("V.dat",2,2);
+
+
+
+
+
+
+
+
+k_1 =(p[0][0]+p[1][1]+(p[0][0]-p[1][1]))/(2*-F);
+k_2 =(p[0][0]+p[1][1]-(p[0][0]-p[1][1]))/(2*-F);
+printf("%f\n",k_1);
+printf("%f\n",k_2);
+}
+
+
+
+
