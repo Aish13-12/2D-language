a6449d2ccda9737e174630a23b9c9c482db50151
Aish13-12 <54772803+Aish13-12@users.noreply.github.com>
2019-09-15 06:58:36 -0700

    Update pres2.c

--- pres2.c
+++ pres2.c
@@ -10,14 +10,7 @@ double k_1,k_2 ,F;
 double **p;
 F=-51;
 p=loadtxt("V.dat",2,2);
-
-
-
-
-
-
-
-
+  //k_1 and k_2 are eigen values obtained from the characteristic equation
 k_1 =(p[0][0]+p[1][1]+(p[0][0]-p[1][1]))/(2*-F);
 k_2 =(p[0][0]+p[1][1]-(p[0][0]-p[1][1]))/(2*-F);
 printf("%f\n",k_1);
